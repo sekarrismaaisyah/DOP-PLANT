1eeec111e8b5f5c4a83e0268c5790462c62fea4d
unknown <20102172@ittelkom-pwt.ac.id>
2026-01-19 11:10:07 +0800

    kembali

--- resources/views/HazardMotion/admin/fullMaps.blade.php
+++ resources/views/HazardMotion/admin/fullMaps.blade.php
@@ -2516,7 +2516,7 @@
                     <i class="material-icons-outlined">assessment</i>
                     <span>Matriks Prediksi</span>
                 </a>
-                <a href="{{ route('fullmaps') }}">
+                <a href="{{ route('maps.index') }}">
                 <span class="gm-category-item">
                     <i class="material-icons-outlined category-arrow">chevron_right</i>
                 </span>
