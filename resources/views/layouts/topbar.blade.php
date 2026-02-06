eef58f4726e6a909a501dc4387c04bdb22513112
unknown <20102172@ittelkom-pwt.ac.id>
2026-02-06 20:24:14 +0800

    tes

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -70,12 +70,12 @@
         
         <li class="nav-item dropdown">
           <a href="javascrpt:;" class="dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
-             <img src="{{ URL::asset('build/images/avatars/01.png') }}" class="rounded-circle p-1 border" width="45" height="45">
+             <img src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png}" class="rounded-circle p-1 border" width="45" height="45">
           </a>
           <div class="dropdown-menu dropdown-user dropdown-menu-end shadow">
             <a class="dropdown-item  gap-2 py-2" href="javascript:;">
               <div class="text-center">
-                <img src="{{ URL::asset('build/images/avatars/01.png') }}" class="rounded-circle p-1 shadow mb-3" width="90" height="90"
+                <img src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png" class="rounded-circle p-1 shadow mb-3" width="90" height="90"
                   alt="">
                 <h5 class="user-name mb-0 fw-bold">Hello, {{ Auth::user()->email ?? 'Guest' }}</h5>
               </div>
